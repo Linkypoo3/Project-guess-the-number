39bbbb5953461fc11f75176773e9ca27ae61414b
Linkypoo3 <120533524+Linkypoo3@users.noreply.github.com>
2023-12-05 19:49:47 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,7 @@
+// generation of random value
+var y=Math.floor(Math.random()*10+1)
+// count of attempts
+// until hit
+  
+// function for the number sent by the user
+var x=document.getElementById("guessField").value
